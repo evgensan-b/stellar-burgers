3bfd3cee09224543b49d13d7caabe2d844c4f53b
EvgenSan <evgnesan@yandex.ru>
2026-03-21 21:30:08 +0300

    feat: add cypress tests

---
+++ cypress/e2e/constructor.cy.tsx
@@ -0,0 +1,87 @@
+describe('Burger Constructor', () => {
+  beforeEach(() => {
+    localStorage.setItem('refreshToken', 'fake-refresh-token');
+    cy.setCookie('accessToken', 'fake-access-token');
+
+    cy.intercept('GET', '**/api/ingredients', {
+      fixture: 'ingredients.json',
+    }).as('getIngredients');
+
+    cy.intercept('GET', '**/api/auth/user', {
+      fixture: 'user.json',
+    }).as('getUser');
+
+    cy.intercept('POST', '**/api/orders', {
+      fixture: 'order.json',
+    }).as('createOrder');
+
+    cy.visit('/');
+    cy.wait('@getIngredients');
+    cy.wait('@getUser');
+  });
+
+  afterEach(() => {
+    cy.clearLocalStorage();
+    cy.clearCookies();
+  });
+
+  it('Добавление ингредиента из списка ингредиентов в конструктор', () => {
+    cy.contains('Краторная булка N-200i').parent().contains('Добавить').click();
+    cy.contains('Биокотлета из марсианской Магнолии')
+      .parent()
+      .contains('Добавить')
+      .click();
+    cy.contains('Соус традиционный галактический')
+      .parent()
+      .contains('Добавить')
+      .click();  
+
+    cy.get('[class*=constructor]').contains('Краторная булка N-200i');
+    cy.get('[class*=constructor]').contains(
+      'Биокотлета из марсианской Магнолии'
+    );
+    cy.get('[class*=constructor]').contains(
+      'Соус традиционный галактический'
+    );
+  });
+
+  it('Открытие и закрытие модального окна ингредиента по клику на крестик', () => {
+    cy.contains('Краторная булка N-200i').click();
+  
+    cy.contains('Детали ингредиента').should('be.visible');
+    cy.contains('Краторная булка N-200i').should('be.visible');
+  
+    cy.contains('Калории, ккал').should('be.visible');
+    cy.contains('Белки, г').should('be.visible');
+    cy.contains('Жиры, г').should('be.visible');
+    cy.contains('Углеводы, г').should('be.visible');
+  
+    cy.get('button.Z7mUFPBZScxutAKTLKHN').click();
+    cy.contains('Детали ингредиента').should('not.exist');
+  });
+
+  it('Открытие и закрытие модального окна по клику на оверлей', () => {
+    cy.contains('Краторная булка N-200i').click();
+    cy.contains('Детали ингредиента').should('be.visible');
+  
+    cy.get('body').click(0, 0);
+    cy.contains('Детали ингредиента').should('not.exist');
+  });
+
+  it('Создание заказа авторизованным пользователем', () => {  
+    cy.contains('Краторная булка N-200i').parent().contains('Добавить').click();
+    cy.contains('Биокотлета из марсианской Магнолии').parent().contains('Добавить').click();
+    
+    cy.contains('Оформить заказ').click();
+    
+    cy.wait('@createOrder');
+    
+    cy.contains('12345').should('be.visible');
+    cy.contains('идентификатор заказа').should('be.visible');
+    
+    cy.get('button.Z7mUFPBZScxutAKTLKHN').click();
+    
+    cy.contains('Выберите булки').should('be.visible');
+    cy.contains('Выберите начинку').should('be.visible');
+  });
+});
